92aea92ab9ec8f6b96fdcfe73b3fc477e5bea961
Hunter Holland <huntholland@gmail.com>
2020-03-28 22:45:02 -0400

    Adding new activity

--- app/src/main/java/com/example/gradeassist/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/gradeassist/ui/home/HomeViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.gradeassist.ui.home
 
+import android.content.Intent
+import android.view.View
+import android.widget.EditText
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,7 +10,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Welcome to GradeAssist"
     }
     val text: LiveData<String> = _text
 }
---
+++ app/src/main/java/com/example/gradeassist/ui/quiz_results/QuizResults.kt
@@ -0,0 +1,13 @@
+package com.example.gradeassist.ui.quiz_results
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gradeassist.R
+
+class QuizResults : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_results)
+    }
+}
