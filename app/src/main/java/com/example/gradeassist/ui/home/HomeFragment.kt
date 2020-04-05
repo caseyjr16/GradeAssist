c6718b309bd0661a1026cf5a9df0ad8d11092654
Rahul <rahulmbme@gmail.com>
2020-04-05 23:17:45 +0530

    My code review suggestions

--- app/src/main/java/com/example/gradeassist/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gradeassist/ui/home/HomeFragment.kt
@@ -4,21 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.gradeassist.R
+import com.example.gradeassist.ui.quiz_results.QuizResultsFragment
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
-    override fun onCreateView(
+    override fun onCreateView
+                (
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View?
+    {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
