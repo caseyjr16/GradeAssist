082af3dadc6d8bc0ef8afa6c0a9abd198eb05ab9
Hunter Holland <huntholland@gmail.com>
2020-04-11 11:59:21 -0400

    Adding local changes

--- app/src/main/java/com/example/gradeassist/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gradeassist/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -21,7 +22,7 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.home_fragment, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(this, Observer {
             textView.text = it
