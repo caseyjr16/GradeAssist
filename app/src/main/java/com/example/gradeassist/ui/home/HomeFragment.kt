e4c8963aafe2e3d2d01ad7ee3ae69a7076d6edc4
Hunter Holland <huntholland@gmail.com>
2020-04-16 15:26:26 -0400

    Event opens on button press

--- app/src/main/java/com/example/gradeassist/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gradeassist/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gradeassist.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.gradeassist.R
+import com.example.gradeassist.ui.quiz_results.ShowQuizAcitivity
 
 class HomeFragment : Fragment() {
 
